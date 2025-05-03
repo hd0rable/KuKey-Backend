51b08cc944b616c2f8ae31d00580c78f2f28cddf
강희진 <kanghuijin12@gmail.com>
2025-05-03 19:14:57 +0900

    init

---
+++ src/main/java/com/example/Kukey_Backend/KukeyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.Kukey_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KukeyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KukeyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Kukey_Backend/KukeyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Kukey_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KukeyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
