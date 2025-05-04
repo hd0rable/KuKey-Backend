ee8ab21ce4cde44fdf49e795a2c0ec873cd3966f
강희진 <kanghuijin12@gmail.com>
2025-05-05 01:56:15 +0900

    [feat] INVALID_BUILDING_TYPE 추가 (#2)

--- src/main/java/com/example/Kukey_Backend/global/response/status/BaseExceptionResponseStatus.java
+++ src/main/java/com/example/Kukey_Backend/global/response/status/BaseExceptionResponseStatus.java
@@ -18,7 +18,8 @@ public enum BaseExceptionResponseStatus implements ResponseStatus {
     /**
      * 1000 : room 관련
      */
-    CANNOT_FOUND_ROOM(1000, BAD_REQUEST, "해당하는 실습실을 찾을 수 없습니다.");
+    CANNOT_FOUND_ROOM(1000, BAD_REQUEST, "해당하는 실습실을 찾을 수 없습니다."),
+    INVALID_BUILDING_TYPE(1001, BAD_REQUEST, "알맞은 건물 이름을 찾을 수 없습니다.");
 
     private final int code;
     private final HttpStatus status;
