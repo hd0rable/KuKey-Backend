83e9217ed9c2f1267861be436a660a24ecce3244
강희진 <kanghuijin12@gmail.com>
2025-05-05 03:30:26 +0900

    [chore] import문 정리 (#2)

--- src/main/java/com/example/Kukey_Backend/domain/room/domain/repository/RoomRepository.java
+++ src/main/java/com/example/Kukey_Backend/domain/room/domain/repository/RoomRepository.java
@@ -1,6 +1,5 @@
 package com.example.Kukey_Backend.domain.room.domain.repository;
 
-import com.example.Kukey_Backend.domain.openRequest.domain.OpenRequest;
 import com.example.Kukey_Backend.domain.room.domain.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
