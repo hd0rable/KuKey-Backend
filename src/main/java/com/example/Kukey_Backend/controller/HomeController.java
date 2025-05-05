8f4def1f68a74f8ced1b3c406949ca9e8557742c
강희진 <kanghuijin12@gmail.com>
2025-05-05 23:12:29 +0900

    [chore] 중복 제거

--- src/main/java/com/example/Kukey_Backend/controller/HomeController.java
+++ src/main/java/com/example/Kukey_Backend/controller/HomeController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping()
     public BaseResponse<String> healthCheck() {
         return BaseResponse.ok("Server is running!");
     }
