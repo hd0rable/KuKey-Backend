30351633eb70da98ea97a876ad8f2d44c7475225
강희진 <kanghuijin12@gmail.com>
2025-05-07 16:58:08 +0900

    [feat] AuthController.sendCode 작성 (#3)

--- src/main/java/com/example/Kukey_Backend/controller/AuthController.java
+++ src/main/java/com/example/Kukey_Backend/controller/AuthController.java
@@ -1,6 +1,12 @@
 package com.example.Kukey_Backend.controller;
 
+import com.example.Kukey_Backend.domain.auth.domain.dto.request.PostAuthSendCodeRequest;
+import com.example.Kukey_Backend.domain.auth.service.AuthService;
+import com.example.Kukey_Backend.global.response.BaseResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/auth")
 public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/code")
+    public BaseResponse<Void> sendCode(@Valid @RequestBody final PostAuthSendCodeRequest postAuthSendCodeRequest) {
+        return BaseResponse.ok(authService.sendCode(postAuthSendCodeRequest));
+    }
 
 }
