c2ea782ad4a31965c134116fc614145320386956
강희진 <kanghuijin12@gmail.com>
2025-05-08 02:56:45 +0900

    [feat] AuthController.verifiedCode 작성 (#4)

--- src/main/java/com/example/Kukey_Backend/controller/AuthController.java
+++ src/main/java/com/example/Kukey_Backend/controller/AuthController.java
@@ -1,8 +1,11 @@
 package com.example.Kukey_Backend.controller;
 
 import com.example.Kukey_Backend.domain.auth.domain.dto.request.PostAuthSendCodeRequest;
+import com.example.Kukey_Backend.domain.auth.domain.dto.request.PostAuthVerifiedCodeRequest;
+import com.example.Kukey_Backend.domain.auth.domain.dto.response.PostAuthSendCodeResponse;
 import com.example.Kukey_Backend.domain.auth.service.AuthService;
 import com.example.Kukey_Backend.global.response.BaseResponse;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,9 +20,16 @@ public class AuthController {
 
     private final AuthService authService;
 
+    //이메일 인증코드 전송
     @PostMapping("/code")
-    public BaseResponse<Void> sendCode(@Valid @RequestBody final PostAuthSendCodeRequest postAuthSendCodeRequest) {
-        return BaseResponse.ok(authService.sendCode(postAuthSendCodeRequest));
+    public BaseResponse<PostAuthSendCodeResponse> sendCode(@Valid @RequestBody final PostAuthSendCodeRequest postAuthSendCodeRequest, HttpServletResponse response) {
+        return BaseResponse.ok(authService.sendCode(postAuthSendCodeRequest, response));
+    }
+
+    //이메일 인증코드 검증
+    @PostMapping("/verification")
+    public BaseResponse<Void> verifiedCode(@Valid @RequestBody final PostAuthVerifiedCodeRequest postAuthVerifiedCodeRequest, HttpServletResponse response) {
+        return BaseResponse.ok(authService.verifyCode(postAuthVerifiedCodeRequest,response));
     }
 
 }
