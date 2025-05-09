1b8e826a1fef96520004f36a91fbfe552833b6e6
강희진 <kanghuijin12@gmail.com>
2025-05-09 19:08:44 +0900

    [feat] OpenRequest 관련 예외코드 추가 (#7)

--- src/main/java/com/example/Kukey_Backend/global/response/status/BaseExceptionResponseStatus.java
+++ src/main/java/com/example/Kukey_Backend/global/response/status/BaseExceptionResponseStatus.java
@@ -28,7 +28,13 @@ public enum BaseExceptionResponseStatus implements ResponseStatus {
     CANNOT_FOUND_TOKEN(2002, UNAUTHORIZED, "토큰을 찾을 수 없습니다."),
     ACCESS_DENIED(2003, UNAUTHORIZED, "접근 할 수 있는 권한이 없습니다."),
     INVALID_AUTH_CODE(2004, BAD_REQUEST, "유효하지 않은 인증코드입니다."),
-    LOGIN_FAILED(2004, BAD_REQUEST, "아이디와 비밀번호가 일치하지 않습니다.");
+    LOGIN_FAILED(2004, BAD_REQUEST, "아이디와 비밀번호가 일치하지 않습니다."),
+
+    /**
+     * 2000 : openRequest 관련
+     */
+    ALREADY_OPEN(3000, BAD_REQUEST, "이미 열려있는 실습실입니다"),
+    ALREADY_OPEN_REQUESTED(3001, BAD_REQUEST, "이미 개방요청이 되어있는 실습실입니다");
 
 
     private final int code;
