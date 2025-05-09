1bb470f690f558948cd5d09c1635918f129e1504
강희진 <kanghuijin12@gmail.com>
2025-05-09 19:11:35 +0900

    [feat] OpenRequestController.openRequest 작성 (#7)

--- src/main/java/com/example/Kukey_Backend/controller/OpenRequestController.java
+++ src/main/java/com/example/Kukey_Backend/controller/OpenRequestController.java
@@ -1,11 +1,21 @@
 package com.example.Kukey_Backend.controller;
 
+import com.example.Kukey_Backend.domain.openRequest.domain.dto.response.PatchOpenRequestResponse;
+import com.example.Kukey_Backend.domain.openRequest.service.OpenRequestService;
+import com.example.Kukey_Backend.global.response.BaseResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/open-requests")
 public class OpenRequestController {
+
+    private final OpenRequestService openRequestService;
+
+    //개방요청하기
+    @PatchMapping("/{spaceId}")
+    public BaseResponse<PatchOpenRequestResponse> openRequest(@PathVariable("spaceId") final Long spaceId) {
+        return BaseResponse.ok(openRequestService.openRequest(spaceId));
+    }
 }
