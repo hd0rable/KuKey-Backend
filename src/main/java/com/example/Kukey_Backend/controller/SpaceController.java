e0d8366bfba06c26b17872796e84b9ce38958da7
강희진 <kanghuijin12@gmail.com>
2025-05-09 19:56:32 +0900

    [feat] SpaceController.getSpacesOpenInfo 작성 (#8)

--- src/main/java/com/example/Kukey_Backend/controller/SpaceController.java
+++ src/main/java/com/example/Kukey_Backend/controller/SpaceController.java
@@ -1,6 +1,10 @@
 package com.example.Kukey_Backend.controller;
 
+import com.example.Kukey_Backend.domain.space.domain.dto.response.GetSpacesOpenInfoResponse;
+import com.example.Kukey_Backend.domain.space.service.SpaceService;
+import com.example.Kukey_Backend.global.response.BaseResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/spaces")
 public class SpaceController {
+
+    private final SpaceService spaceService;
+
+    @GetMapping("/open")
+    public BaseResponse<GetSpacesOpenInfoResponse> getSpacesOpenInfo(){
+        return BaseResponse.ok(spaceService.getSpacesOpenInfo());
+    }
 }
