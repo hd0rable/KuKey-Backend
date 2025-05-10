326b14e3e00b8eea8c49e58cb6fd7583a95ece8d
강희진 <kanghuijin12@gmail.com>
2025-05-10 16:55:33 +0900

    [feat] AdminController.getSpacesOpenInfo 작성 (#10)

--- src/main/java/com/example/Kukey_Backend/controller/AdminController.java
+++ src/main/java/com/example/Kukey_Backend/controller/AdminController.java
@@ -1,11 +1,24 @@
 package com.example.Kukey_Backend.controller;
 
+import com.example.Kukey_Backend.domain.space.domain.dto.response.GetSpacesOpenInfoResponse;
+import com.example.Kukey_Backend.domain.space.service.SpaceService;
+import com.example.Kukey_Backend.global.annotation.RoleRequired;
+import com.example.Kukey_Backend.global.response.BaseResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/admin")
+@RoleRequired("ADMIN")
 public class AdminController {
+
+    private final SpaceService spaceService;
+
+    @GetMapping("/spaces")
+    public BaseResponse<GetSpacesOpenInfoResponse> getSpacesOpenInfo(){
+        return BaseResponse.ok(spaceService.getSpacesOpenInfo());
+    }
 }
