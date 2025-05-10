084625c0c6455bb13b7a35f2891d3b466414b6ab
강희진 <kanghuijin12@gmail.com>
2025-05-10 22:45:26 +0900

    [feat] BuildingName.from 작성 (#13)

--- src/main/java/com/example/Kukey_Backend/domain/space/domain/BuildingName.java
+++ src/main/java/com/example/Kukey_Backend/domain/space/domain/BuildingName.java
@@ -1,6 +1,9 @@
 package com.example.Kukey_Backend.domain.space.domain;
 
+import com.example.Kukey_Backend.global.exception.GlobalException;
 import lombok.Getter;
+
+import static com.example.Kukey_Backend.global.response.status.BaseExceptionResponseStatus.INVALID_BUILDING_TYPE;
 
 @Getter
 public enum BuildingName {
@@ -13,6 +16,15 @@ public enum BuildingName {
 
     BuildingName(String buildingName) {
         this.buildingName = buildingName;
+    }
+
+    public static BuildingName from(String value) {
+        for (BuildingName buildingName : BuildingName.values()) {
+            if (buildingName.getBuildingName().equals(value)) {
+                return buildingName;
+            }
+        }
+        throw new GlobalException(INVALID_BUILDING_TYPE);
     }
 
 }
