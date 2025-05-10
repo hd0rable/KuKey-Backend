d257b167308ad717037a3c9ad247b8905d28a36e
강희진 <kanghuijin12@gmail.com>
2025-05-10 23:23:13 +0900

    [feat] KeyLocationService.getKeyLocationInfo 작성 (#14)

--- src/main/java/com/example/Kukey_Backend/domain/keyLocation/service/KeyLocationService.java
+++ src/main/java/com/example/Kukey_Backend/domain/keyLocation/service/KeyLocationService.java
@@ -2,6 +2,7 @@ package com.example.Kukey_Backend.domain.keyLocation.service;
 
 import com.example.Kukey_Backend.domain.keyLocation.domain.KeyLocation;
 import com.example.Kukey_Backend.domain.keyLocation.domain.dto.request.PostKeyLocationUploadRequest;
+import com.example.Kukey_Backend.domain.keyLocation.domain.dto.response.GetKeyLocationInfoResponse;
 import com.example.Kukey_Backend.domain.keyLocation.domain.dto.response.PostKeyLocationUploadResponse;
 import com.example.Kukey_Backend.domain.keyLocation.domain.repository.KeyLocationRepository;
 import com.example.Kukey_Backend.domain.space.domain.BuildingName;
@@ -14,6 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+import static com.example.Kukey_Backend.global.response.status.BaseExceptionResponseStatus.CANNOT_FOUND_KEY_LOCATION;
 import static com.example.Kukey_Backend.global.response.status.BaseExceptionResponseStatus.CANNOT_FOUND_SPACE;
 
 @Service
@@ -54,6 +56,28 @@ public class KeyLocationService {
         return PostKeyLocationUploadResponse.builder()
                 .buildingName(postKeyLocationUploadRequest.buildingName())
                 .keyLocationId(keyLocationRepository.findBySpace(spaceList.get(0)).get().getKeyLocationId())
+                .build();
+
+    }
+
+    //카드키 위치 기록 조회
+    @Transactional(readOnly = true)
+    public GetKeyLocationInfoResponse getKeyLocationInfo(String buildingName) {
+
+        BuildingName EnumbuildingName = BuildingName.from(buildingName);
+
+        Space space = spaceRepository.findFirstByBuildingNameOrderBySpaceIdAsc(EnumbuildingName)
+                .orElseThrow(() -> new GlobalException(CANNOT_FOUND_SPACE));
+
+        KeyLocation keyLocation = keyLocationRepository.findBySpace(space)
+                .orElseThrow(() -> new GlobalException(CANNOT_FOUND_KEY_LOCATION));
+
+        return GetKeyLocationInfoResponse.builder()
+                .keyLocationId(keyLocation.getKeyLocationId())
+                .buildingName(buildingName)
+                .adminName(keyLocation.getAdminName())
+                .imageUrl(keyLocation.getImageUrl())
+                .description(keyLocation.getDescription())
                 .build();
 
     }
