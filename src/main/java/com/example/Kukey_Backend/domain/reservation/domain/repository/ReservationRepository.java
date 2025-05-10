4b1f5eecfb37c4f09b68e940b483f7e3f08f5ae4
강희진 <kanghuijin12@gmail.com>
2025-05-11 01:24:03 +0900

    [feat] ReservationRepository.findAllBySpaceAndReservationDate 작성 (#15)

--- src/main/java/com/example/Kukey_Backend/domain/reservation/domain/repository/ReservationRepository.java
+++ src/main/java/com/example/Kukey_Backend/domain/reservation/domain/repository/ReservationRepository.java
@@ -1,10 +1,15 @@
 package com.example.Kukey_Backend.domain.reservation.domain.repository;
 
 import com.example.Kukey_Backend.domain.reservation.domain.Reservation;
+import com.example.Kukey_Backend.domain.space.domain.Space;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
+    List<Reservation> findAllBySpaceAndReservationDate(Space space, LocalDate localDate);
 }
