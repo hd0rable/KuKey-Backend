f1abcef73e0452a4bd1e90811886c72aa1a407f3
강희진 <kanghuijin12@gmail.com>
2025-05-11 01:24:20 +0900

    [feat] ReservationService.reservationToSpace 작성 (#15)

---
+++ src/main/java/com/example/Kukey_Backend/domain/reservation/service/ReservationService.java
@@ -0,0 +1,81 @@
+package com.example.Kukey_Backend.domain.reservation.service;
+
+import com.example.Kukey_Backend.domain.reservation.domain.Reservation;
+import com.example.Kukey_Backend.domain.reservation.domain.dto.request.PostReservationToSpaceRequest;
+import com.example.Kukey_Backend.domain.reservation.domain.repository.ReservationRepository;
+import com.example.Kukey_Backend.domain.space.domain.Space;
+import com.example.Kukey_Backend.domain.space.domain.repository.SpaceRepository;
+import com.example.Kukey_Backend.global.exception.GlobalException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Duration;
+import java.time.LocalTime;
+import java.util.List;
+
+import static com.example.Kukey_Backend.global.entity.RequestStatus.APPROVED;
+import static com.example.Kukey_Backend.global.response.status.BaseExceptionResponseStatus.*;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ReservationService {
+
+    private final SpaceRepository spaceRepository;
+    private final ReservationRepository reservationRepository;
+
+    /**
+     * 실습실 예약하기
+     */
+    public Void reservationToSpace(Long spaceId, PostReservationToSpaceRequest postReservationToSpaceRequest) {
+
+        LocalTime allowedStart = LocalTime.of(9, 0); // 오전 9시
+        LocalTime allowedEnd = LocalTime.of(22, 0); // 오후 10시
+
+        // 예약 시간이 허용 범위(09:00~22:00)를 벗어나는 경우 예외
+        if (postReservationToSpaceRequest.reservationStartTime().isBefore(allowedStart)) {
+            throw new GlobalException(RESERVATION_TIME_INVALID);
+        }
+        if (postReservationToSpaceRequest.reservationEndTime().isAfter(allowedEnd)) {
+            throw new GlobalException(RESERVATION_TIME_INVALID);
+        }
+
+        // 예약 시간 3시간 이상 예외처리
+        Duration duration = Duration.between(postReservationToSpaceRequest.reservationStartTime(), postReservationToSpaceRequest.reservationEndTime());
+        if (duration.toHours() > 3 || duration.isNegative() || duration.isZero()) {
+            throw new GlobalException(RESERVATION_TIME_TOO_LONG);
+        }
+
+        Space space = spaceRepository.findById(spaceId)
+                .orElseThrow(() -> new GlobalException(CANNOT_FOUND_SPACE));
+
+        // 예약 시간 겹침 예외처리
+        List<Reservation> existingReservations = reservationRepository
+                .findAllBySpaceAndReservationDate(space,postReservationToSpaceRequest.reservationDate());
+
+        boolean isOverlap = existingReservations.stream().anyMatch(res ->
+                !(postReservationToSpaceRequest.reservationEndTime().isBefore(res.getReservationStartTime()) ||
+                        postReservationToSpaceRequest.reservationStartTime().isAfter(res.getReservationEndTime()))
+        );
+        if (isOverlap) {
+            throw new GlobalException(DUPLICATE_RESERVATION_TIME);
+        }
+
+        Reservation reservation = Reservation.builder()
+                .space(space)
+                .reservationDate(postReservationToSpaceRequest.reservationDate())
+                .reservationStartTime(postReservationToSpaceRequest.reservationStartTime())
+                .reservationEndTime(postReservationToSpaceRequest.reservationEndTime())
+                .studentNumber(postReservationToSpaceRequest.studentNumber())
+                .studentName(postReservationToSpaceRequest.studentName())
+                .studentGroup(postReservationToSpaceRequest.studentGroup())
+                .reservationPurpose(postReservationToSpaceRequest.reservationPurpose())
+                .status(APPROVED)
+                .build();
+
+        reservationRepository.save(reservation);
+        return null;
+    }
+
+}
