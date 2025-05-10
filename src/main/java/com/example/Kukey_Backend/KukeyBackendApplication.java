1816f7b9f50005625b7877745f346fba7adeb13f
강희진 <kanghuijin12@gmail.com>
2025-05-10 15:45:00 +0900

    [feat] @ EnableFeignClients 추가(#9)

--- src/main/java/com/example/Kukey_Backend/KukeyBackendApplication.java
+++ src/main/java/com/example/Kukey_Backend/KukeyBackendApplication.java
@@ -2,12 +2,14 @@ package com.example.Kukey_Backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableAspectJAutoProxy
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableFeignClients
 public class KukeyBackendApplication {
 
 	public static void main(String[] args)
