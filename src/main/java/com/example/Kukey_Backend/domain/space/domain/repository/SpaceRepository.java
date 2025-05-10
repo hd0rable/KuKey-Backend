9d456a861fba003aecd7e62fc4530c834d433392
강희진 <kanghuijin12@gmail.com>
2025-05-10 22:46:42 +0900

    [feat] SpaceRepository.findAllByBuildingName 작성 (#13)

--- src/main/java/com/example/Kukey_Backend/domain/space/domain/repository/SpaceRepository.java
+++ src/main/java/com/example/Kukey_Backend/domain/space/domain/repository/SpaceRepository.java
@@ -1,10 +1,15 @@
 package com.example.Kukey_Backend.domain.space.domain.repository;
 
+import com.example.Kukey_Backend.domain.space.domain.BuildingName;
 import com.example.Kukey_Backend.domain.space.domain.Space;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface SpaceRepository extends JpaRepository<Space, Long> {
 
+    Optional<List<Space>> findAllByBuildingName(BuildingName buildingName);
 }
