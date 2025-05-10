05c78724c2b1c99a7db2900c37c99542fe104901
강희진 <kanghuijin12@gmail.com>
2025-05-11 02:09:00 +0900

    [feat] ReservationInfo dto 작성 (#16)

---
+++ src/main/java/com/example/Kukey_Backend/domain/reservation/domain/dto/response/ReservationInfo.java
@@ -0,0 +1,19 @@
+package com.example.Kukey_Backend.domain.reservation.domain.dto.response;
+
+import lombok.Builder;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Builder
+public record ReservationInfo(
+        Long spaceId,
+        String buildingName,
+        String spaceDisplayName,
+        LocalDate reservationDate,
+        LocalTime reservationStartTime,
+        LocalTime reservationEndTime,
+        String studentGroup,
+        String reservationPurpose
+) {
+}
