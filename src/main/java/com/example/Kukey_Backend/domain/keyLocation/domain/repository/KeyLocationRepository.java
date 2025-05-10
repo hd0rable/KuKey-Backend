bbeb7a238719ac5a3d858719ca375478ff8233dc
강희진 <kanghuijin12@gmail.com>
2025-05-10 22:45:41 +0900

    [feat]  KeyLocationRepository.findBySpace 작성 (#13)

--- src/main/java/com/example/Kukey_Backend/domain/keyLocation/domain/repository/KeyLocationRepository.java
+++ src/main/java/com/example/Kukey_Backend/domain/keyLocation/domain/repository/KeyLocationRepository.java
@@ -1,9 +1,13 @@
 package com.example.Kukey_Backend.domain.keyLocation.domain.repository;
 
 import com.example.Kukey_Backend.domain.keyLocation.domain.KeyLocation;
+import com.example.Kukey_Backend.domain.space.domain.Space;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface KeyLocationRepository extends JpaRepository<KeyLocation, Long> {
+    Optional<KeyLocation> findBySpace(Space space);
 }
