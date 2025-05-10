80241fd7dcca025b5df3669a00f200a369c64cdc
강희진 <kanghuijin12@gmail.com>
2025-05-10 23:23:24 +0900

    [feat] SpaceRepository.findFirstByBuildingNameOrderBySpaceIdAsc 작성 (#14)

--- src/main/java/com/example/Kukey_Backend/domain/space/domain/repository/SpaceRepository.java
+++ src/main/java/com/example/Kukey_Backend/domain/space/domain/repository/SpaceRepository.java
@@ -12,4 +12,6 @@ import java.util.Optional;
 public interface SpaceRepository extends JpaRepository<Space, Long> {
 
     Optional<List<Space>> findAllByBuildingName(BuildingName buildingName);
+    Optional<Space> findFirstByBuildingNameOrderBySpaceIdAsc(BuildingName buildingName);
+
 }
