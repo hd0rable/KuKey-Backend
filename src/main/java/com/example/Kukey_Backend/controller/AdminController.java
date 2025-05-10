90405c8c06c3dfb0ef4bd8363cd3aeb057ca6cc9
강희진 <kanghuijin12@gmail.com>
2025-05-10 22:43:44 +0900

    [chore] 컨트롤러 주석 추가

--- src/main/java/com/example/Kukey_Backend/controller/AdminController.java
+++ src/main/java/com/example/Kukey_Backend/controller/AdminController.java
@@ -2,6 +2,7 @@ package com.example.Kukey_Backend.controller;
 
 import com.example.Kukey_Backend.domain.admin.domain.dto.response.PatchOpenChangeResponse;
 import com.example.Kukey_Backend.domain.admin.service.AdminService;
+import com.example.Kukey_Backend.domain.keyLocation.service.KeyLocationService;
 import com.example.Kukey_Backend.domain.notification.service.S3Service;
 import com.example.Kukey_Backend.domain.space.domain.dto.response.GetSpacesOpenInfoResponse;
 import com.example.Kukey_Backend.domain.space.service.SpaceService;
@@ -18,22 +19,28 @@ import org.springframework.web.multipart.MultipartFile;
 public class AdminController {
 
     private final SpaceService spaceService;
+    private final KeyLocationService keyLocationService;
     private final AdminService adminService;
     private final S3Service s3Service;
 
+    //관리자 개방 관리 조회
     @GetMapping("/spaces")
     public BaseResponse<GetSpacesOpenInfoResponse> getSpacesOpenInfo(){
         return BaseResponse.ok(spaceService.getSpacesOpenInfo());
     }
 
+    //관리자 실습실 관리 (잠금 개방 상태 변경)
     @PatchMapping("/open-change/{spaceId}")
     public BaseResponse<PatchOpenChangeResponse> changeSpaceOpenStatus(@PathVariable("spaceId") Long spaceId){
         return  BaseResponse.ok(adminService.changeSpaceOpenStatus(spaceId));
     }
 
-    @PostMapping("key/images")
+    //카드키 이미지 업로드
+    @PostMapping("/key/images")
     public BaseResponse<?> s3ImageUpload(@RequestPart(value = "image", required = false) MultipartFile image){
         return BaseResponse.ok(s3Service.getImageFromUser(image));
     }
+
+
 
 }
