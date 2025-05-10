778fb4242fbdfd4d46bda4afdb0c964e069d1ee7
강희진 <kanghuijin12@gmail.com>
2025-05-10 17:35:59 +0900

    [feat] AdminController.changeSpaceOpenStatus 작성 (#11)

--- src/main/java/com/example/Kukey_Backend/controller/AdminController.java
+++ src/main/java/com/example/Kukey_Backend/controller/AdminController.java
@@ -1,13 +1,13 @@
 package com.example.Kukey_Backend.controller;
 
+import com.example.Kukey_Backend.domain.admin.domain.dto.response.PatchOpenChangeResponse;
+import com.example.Kukey_Backend.domain.admin.service.AdminService;
 import com.example.Kukey_Backend.domain.space.domain.dto.response.GetSpacesOpenInfoResponse;
 import com.example.Kukey_Backend.domain.space.service.SpaceService;
 import com.example.Kukey_Backend.global.annotation.RoleRequired;
 import com.example.Kukey_Backend.global.response.BaseResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,9 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class AdminController {
 
     private final SpaceService spaceService;
+    private final AdminService adminService;
 
     @GetMapping("/spaces")
     public BaseResponse<GetSpacesOpenInfoResponse> getSpacesOpenInfo(){
         return BaseResponse.ok(spaceService.getSpacesOpenInfo());
+    }
+
+    @PatchMapping("/open-change/{spaceId}")
+    public BaseResponse<PatchOpenChangeResponse> changeSpaceOpenStatus(@PathVariable("spaceId") Long spaceId){
+        return  BaseResponse.ok(adminService.changeSpaceOpenStatus(spaceId));
     }
 }
