c3ac9b13c867903bd2219b543392ca9ac05880ea
강희진 <kanghuijin12@gmail.com>
2025-05-12 15:54:37 +0900

    [feat] @ EnableScheduling       @ EnableAsync 어노테이션 추가 (#18)

--- src/main/java/com/example/Kukey_Backend/KukeyBackendApplication.java
+++ src/main/java/com/example/Kukey_Backend/KukeyBackendApplication.java
@@ -5,11 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableAspectJAutoProxy
-@EnableJpaAuditing
 @SpringBootApplication
 @EnableFeignClients
+@EnableAspectJAutoProxy
+@EnableJpaAuditing
+@EnableScheduling
+@EnableAsync
 public class KukeyBackendApplication {
 
 	public static void main(String[] args)
