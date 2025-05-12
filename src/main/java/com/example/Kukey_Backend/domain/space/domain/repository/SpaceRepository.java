9e0cb8cfa51468ac48bfb917b152c09423f68768
강희진 <kanghuijin12@gmail.com>
2025-05-12 15:55:39 +0900

    [feat] SpaceRepository.findByReservationStatus 작성 (#18)

--- src/main/java/com/example/Kukey_Backend/domain/space/domain/repository/SpaceRepository.java
+++ src/main/java/com/example/Kukey_Backend/domain/space/domain/repository/SpaceRepository.java
@@ -2,6 +2,7 @@ package com.example.Kukey_Backend.domain.space.domain.repository;
 
 import com.example.Kukey_Backend.domain.space.domain.BuildingName;
 import com.example.Kukey_Backend.domain.space.domain.Space;
+import com.example.Kukey_Backend.global.entity.RequestStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,5 +14,5 @@ public interface SpaceRepository extends JpaRepository<Space, Long> {
 
     Optional<List<Space>> findAllByBuildingName(BuildingName buildingName);
     Optional<Space> findFirstByBuildingNameOrderBySpaceIdAsc(BuildingName buildingName);
-
+    List<Space> findByReservationStatus(RequestStatus reservationStatus);
 }
