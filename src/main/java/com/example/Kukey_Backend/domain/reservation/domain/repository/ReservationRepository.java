8e464aa1a5c62224b1b53181b7e828652fa4155d
강희진 <kanghuijin12@gmail.com>
2025-05-12 15:55:04 +0900

    [feat] ReservationRepository 관련 함수 작성 (#18)

--- src/main/java/com/example/Kukey_Backend/domain/reservation/domain/repository/ReservationRepository.java
+++ src/main/java/com/example/Kukey_Backend/domain/reservation/domain/repository/ReservationRepository.java
@@ -3,9 +3,12 @@ package com.example.Kukey_Backend.domain.reservation.domain.repository;
 import com.example.Kukey_Backend.domain.reservation.domain.Reservation;
 import com.example.Kukey_Backend.domain.space.domain.Space;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 @Repository
@@ -15,4 +18,15 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
 
     List<Reservation> findByStudentNumberAndStudentNameOrderByReservationDateAsc(String studentNumber, String studentName);
 
+    // 활성 예약 조회 (시작 ≤ 현재 < 종료)
+    @Query("SELECT r FROM Reservation r " +
+            "WHERE r.reservationStartTime <= :now " +
+            "AND r.reservationEndTime > :now ")
+    List<Reservation> findByReservationStartTimeLessThanEqualAndReservationEndTimeAfter(@Param("now") LocalTime now);
+
+    boolean existsBySpaceAndReservationStartTimeLessThanEqualAndReservationEndTimeAfter(Space space,
+                                                                                                 LocalTime nowStart,
+                                                                                                 LocalTime nowEnd);
+
+    List<Reservation> findByReservationDateBefore(LocalDate date);
 }
