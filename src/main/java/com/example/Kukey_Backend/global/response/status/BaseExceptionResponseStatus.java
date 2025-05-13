45c7f286d7e00aac32bffe8229e93ad0f43a3dfc
강희진 <kanghuijin12@gmail.com>
2025-05-13 14:25:53 +0900

    [feat] 에약취소 예외관련 예외코드 추가 (#19)

--- src/main/java/com/example/Kukey_Backend/global/response/status/BaseExceptionResponseStatus.java
+++ src/main/java/com/example/Kukey_Backend/global/response/status/BaseExceptionResponseStatus.java
@@ -62,7 +62,8 @@ public enum BaseExceptionResponseStatus implements ResponseStatus {
     RESERVATION_TIME_INVALID(7002,BAD_REQUEST, "예약은 오전 9시부터 오후 10시까지 가능합니다."),
     RESERVATION_DATE_INVALID(7003,BAD_REQUEST, "예약 날짜는 오늘 이후여야 합니다."),
     CANNOT_FOUND_RESERVATION(7004, BAD_REQUEST, "해당하는 예약 내역을 찾을 수 없습니다"),
-    RESERVATION_DATE_TIME_INVALID(7005,BAD_REQUEST, "예약 시간은 오늘 이후, 현재 시간보다 이후에 가능합니다.");
+    RESERVATION_DATE_TIME_INVALID(7005,BAD_REQUEST, "예약 시간은 오늘 이후, 현재 시간보다 이후에 가능합니다."),
+    CANNOT_DELETE_RESERVATION(7006, BAD_REQUEST, "예약을 취소 할 수 있는 권한이 없습니다");
 
 
 
