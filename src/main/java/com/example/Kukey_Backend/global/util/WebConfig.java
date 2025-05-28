6915ab6cddb2f8655a06e63bd178395f0b73827e
강희진 <kanghuijin12@gmail.com>
2025-05-28 14:01:05 +0900

    [fix] 프론트 배포 주소 추가

--- src/main/java/com/example/Kukey_Backend/global/util/WebConfig.java
+++ src/main/java/com/example/Kukey_Backend/global/util/WebConfig.java
@@ -1,6 +1,5 @@
 package com.example.Kukey_Backend.global.util;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173") // 프론트 주소 명시
+                .allowedOrigins("http://localhost:5173", "https://ku-key-front-end.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowCredentials(true);
     }
